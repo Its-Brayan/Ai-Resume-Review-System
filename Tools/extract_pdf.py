979bb2d69fc3a4e316eab602fd21eedde0f54278
Its-Brayan <brayanmwangi732@gmail.com>
2026-07-07 11:11:13 +0300

    fix:change type to filetype in extract pdf function

--- Tools/extract_pdf.py
+++ Tools/extract_pdf.py
@@ -1,7 +1,7 @@
 import fitz
 
 def extract_pdf(uploaded_file):
-    pdf = fitz.open(stream=uploaded_file.read(),type='pdf')
+    pdf = fitz.open(stream=uploaded_file.read(),filetype='pdf')
     text = ""
     for page in pdf:
         text += page.get_text()
