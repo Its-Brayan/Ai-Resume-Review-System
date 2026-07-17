2c7e52f696fa7589e399eb13140f1a2d827b2693
Its-Brayan <brayanmwangi732@gmail.com>
2026-07-17 15:19:09 +0300

    feat:Changed the document parser to llama index parser

--- app.py
+++ app.py
@@ -119,7 +119,6 @@ if st.session_state.result:
     res = st.session_state.result
    
     st.markdown("### 🧭 Overview")
-    st.markdown(res)
     
     st.markdown(res.get("resume_parser", ""))
 
