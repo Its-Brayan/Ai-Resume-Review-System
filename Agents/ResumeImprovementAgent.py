18ca11ba89a4d82ae93dbbe98fd1ed34d510b655
Its-Brayan <brayanmwangi732@gmail.com>
2026-07-20 15:02:29 +0300

    feat:ATS agent to use gemini model

--- Agents/ResumeImprovementAgent.py
+++ Agents/ResumeImprovementAgent.py
@@ -1,16 +1,19 @@
-from Code.llm import get_groq_llm
+from Code.llm import get_groq_llm, get_gemini_llm
 from Code.paths import RESUME_IMPROVEMENT_AGENT_PROMPT
 from Code.load_yaml import load_config
 from Code.prompt_builder import build_prompt_body
 
 class ResumeImprovementAgent:
-    llm = get_groq_llm('deepseek-r1-distill-llama-70b')
+    llm = get_gemini_llm()
 
     def resume_improver(self,query:str):
         config = load_config(RESUME_IMPROVEMENT_AGENT_PROMPT)
         prompt = build_prompt_body(config['resume_improvement_agent'],query)
-        output = self.llm.invoke(prompt)
+        output = self.llm.models.generate_content(
+            model='gemini-3-flash-preview',
+            contents=prompt
+        )
         result = {
-            "resume_improver":output
+            "resume_improver":output.text
         }
         return result
