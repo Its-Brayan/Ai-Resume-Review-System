e5e7800addd8fd44b3962c640e148be9bc33270f
Its-Brayan <brayanmwangi732@gmail.com>
2026-07-20 14:46:07 +0300

    feat:ATS agent to use gemini model

--- Agents/AtsScoringAgent.py
+++ Agents/AtsScoringAgent.py
@@ -7,7 +7,7 @@ from google.genai import types
 
 
 class AtsScoringAgent:
-    llm = get_groq_llm('deepseek-r1-distill-qwen-32b')
+    llm = get_gemini_llm()
     def ats_scorer(self,resume):
         # resume.seek(0)
         # file_part = types.Part.from_bytes(
@@ -16,7 +16,10 @@ class AtsScoringAgent:
         # )
         config = load_config(ATS_AGENT_PROMPT)
         prompt = build_prompt_body(config['ats_review_agent'],resume)
-        output = self.llm.invoke(prompt)
+        output = self.llm.models.generate_content(
+            model='gemini-3.5-flash',
+            contents=prompt
+        )
         result = {
             'ats_score':output
         }
